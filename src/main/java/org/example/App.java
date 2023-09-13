6a732815b5f9df96b1d0cff1fd8e59874f5dadbb
Nikita Kovalov <nik.kovalev1760@gmail.com>
2023-09-13 04:18:05 -0400

    Finalized

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,26 +1,24 @@
 package org.example;
 
 import org.apache.log4j.Logger;
-import org.flywaydb.core.Flyway;
+import org.example.data.Member;
+import org.example.data.OsbbCRUD;
+import java.io.IOException;
+import java.sql.SQLException;
 
-import static org.example.data.Config.*;
+public class App {
+        private static final Logger logger = Logger.getLogger(org.example.App.class);
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    private static final Logger logger = Logger.getLogger(App.class);
-    public static void main( String[] args )
-    {
-        logger.info("The program has started");
-        logger.debug("Flyway migration execute");
 
-        Flyway.configure()
-                .dataSource(jdbcUrl, username, password)
-                .locations("classpath:osbb/migration")
-                .load()
-                .migrate();
+        public static void main( String[] args ) {
+            logger.info("The program has started");
+
+            try (OsbbCRUD crud = new OsbbCRUD().init()) {
+                for (Member member : crud.getMembersWithAutoNotAllowed()) {
+                    System.out.println(member);
+                }
+            } catch (SQLException | IOException e) {
+                logger.fatal(e);
+            }
+        }
     }
-}
